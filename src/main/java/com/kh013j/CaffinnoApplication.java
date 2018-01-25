411c48a44bc86c910805ccdd782307e77f834c1d
elena-maximenko <amely.honey@gmail.com>
2018-01-25 22:18:59 +0200

    Project initial commit.

---
+++ src/main/java/com/kh013j/CaffinnoApplication.java
@@ -0,0 +1,12 @@
+package com.kh013j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaffinnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaffinnoApplication.class, args);
+	}
+}
