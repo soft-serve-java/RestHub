2a11ee863cf6c7d7cd6e9396ea064bd72767a4f2
elena-maximenko <amely.honey@gmail.com>
2018-01-25 23:37:12 +0200

    Add dependencies for JSP and JSTL.

---
+++ src/main/java/com/kh013j/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package com.kh013j.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by User on 25.01.2018.
+ */
+@Controller
+public class HelloController {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String sayHello(){
+        return "Hello";
+    }
+}
---
+++ src/main/resources/Hello.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h3>Hello, our project name</h3>
+</body>
+</html>
