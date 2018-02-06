2a74f40927214bba88af6425b94c1fad33699632
Arthur <freshsok@gmail.com>
2018-02-06 10:57:42 +0200

    Added Constants for view names.

--- src/main/java/com/kh013j/controllers/util/ViewName.java
+++ src/main/java/com/kh013j/controllers/util/ViewName.java
@@ -1,4 +1,10 @@
 package com.kh013j.controllers.util;
 
 public class ViewName {
+    public static String MENU = "Layoutgrid";
+
+    public static String DISH_DESCRIPTION = "Dishdescription";
+
+    public static String WELCOME = "Welcome";
+
 }
