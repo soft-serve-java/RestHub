af4b38f2f5d1f5f3960fe837b0414d9f12c4d428
Arthur <freshsok@gmail.com>
2018-02-06 18:43:50 +0200

    autoconfig

--- src/main/java/com/kh013j/model/config/JPAConfig.java
+++ src/main/java/com/kh013j/model/config/JPAConfig.java
@@ -23,58 +23,58 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = "com.kh013j.model.repository")
-@PropertySources({
-    @PropertySource("classpath:jpa.properties"),
-    @PropertySource("classpath:local_jpa.properties")})
+//@PropertySources({
+//    @PropertySource("classpath:jpa.properties"),
+//    @PropertySource("classpath:local_jpa.properties")})
 public class JPAConfig {
-  @Autowired
-  private Environment env;
-
-  @Bean
-  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-
-    HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-    vendorAdapter.setDatabase(Database.POSTGRESQL);
-    vendorAdapter.setGenerateDdl(true);
-
-    LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-    em.setDataSource(dataSource());
-    em.setPackagesToScan("com.kh013j.model.domain");
-    em.setJpaVendorAdapter(vendorAdapter);
-    em.setJpaProperties(additionalProperties());
-
-    return em;
-  }
-
-  @Bean
-  public DataSource dataSource(){
-    DriverManagerDataSource dataSource = new DriverManagerDataSource();
-    dataSource.setDriverClassName(env.getProperty("hibernate.connection.driver_class"));
-    dataSource.setUrl(env.getProperty("hibernate.connection.url"));
-    dataSource.setUsername(env.getProperty("hibernate.connection.username"));
-    dataSource.setPassword(env.getProperty("hibernate.connection.password"));
-    return dataSource;
-  }
-
-  @Bean
-  public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
-    JpaTransactionManager transactionManager = new JpaTransactionManager();
-    transactionManager.setEntityManagerFactory(emf);
-    return transactionManager;
-  }
-
-  @Bean
-  public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
-    return new PersistenceExceptionTranslationPostProcessor();
-  }
-
-  Properties additionalProperties() {
-    Properties properties = new Properties();
-    properties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
-    properties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
-    properties.setProperty("hibernate.current_session_context_class",
-        env.getProperty("hibernate.current_session_context_class"));
-    return properties;
-  }
+//  @Autowired
+//  private Environment env;
+//
+//  @Bean
+//  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+//
+//    HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+//    vendorAdapter.setDatabase(Database.POSTGRESQL);
+//    vendorAdapter.setGenerateDdl(true);
+//
+//    LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+//    em.setDataSource(dataSource());
+//    em.setPackagesToScan("com.kh013j.model.domain");
+//    em.setJpaVendorAdapter(vendorAdapter);
+//    em.setJpaProperties(additionalProperties());
+//
+//    return em;
+//  }
+//
+//  @Bean
+//  public DataSource dataSource(){
+//    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//    dataSource.setDriverClassName(env.getProperty("hibernate.connection.driver_class"));
+//    dataSource.setUrl(env.getProperty("hibernate.connection.url"));
+//    dataSource.setUsername(env.getProperty("hibernate.connection.username"));
+//    dataSource.setPassword(env.getProperty("hibernate.connection.password"));
+//    return dataSource;
+//  }
+//
+//  @Bean
+//  public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+//    JpaTransactionManager transactionManager = new JpaTransactionManager();
+//    transactionManager.setEntityManagerFactory(emf);
+//    return transactionManager;
+//  }
+//
+//  @Bean
+//  public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+//    return new PersistenceExceptionTranslationPostProcessor();
+//  }
+//
+//  Properties additionalProperties() {
+//    Properties properties = new Properties();
+//    properties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+//    properties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+//    properties.setProperty("hibernate.current_session_context_class",
+//        env.getProperty("hibernate.current_session_context_class"));
+//    return properties;
+//  }
 
 }
