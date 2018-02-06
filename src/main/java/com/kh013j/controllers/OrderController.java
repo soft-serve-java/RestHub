2a783943c1cd3eedfce2d11e672850e9889f74ad
Arthur <freshsok@gmail.com>
2018-02-06 11:02:23 +0200

    Removed unused imports

--- src/main/java/com/kh013j/controllers/OrderController.java
+++ src/main/java/com/kh013j/controllers/OrderController.java
@@ -10,7 +10,6 @@ import com.kh013j.model.service.interfaces.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
