8af995440a646b5ab4bacdff6d0c0cf5fdd085c9
Olga Rassokha <loony.pax@gmail.com>
2018-02-22 12:13:35 +0300

    Autoconfig

--- src/test/java/com/kh013j/CaffinnoApplicationTests.java
+++ src/test/java/com/kh013j/CaffinnoApplicationTests.java
@@ -2,10 +2,12 @@ package com.kh013j;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@EnableAutoConfiguration
 @SpringBootTest
 public class CaffinnoApplicationTests {
 
