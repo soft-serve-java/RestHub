de40c74f8e882a367d271313eb249d65d8310978
anka-kmbl <anna.maksymenko@nure.ua>
2018-02-28 16:43:15 +0200

    application properties + mockbean

--- src/test/java/com/kh013j/CaffinnoApplicationTestConfiguration.java
+++ src/test/java/com/kh013j/CaffinnoApplicationTestConfiguration.java
@@ -4,9 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.mail.javamail.JavaMailSender;
 import ru.yandex.qatools.embed.postgresql.EmbeddedPostgres;
 
 import java.io.IOException;
@@ -14,6 +16,7 @@ import java.io.IOException;
 import static ru.yandex.qatools.embed.postgresql.distribution.Version.Main.V10;
 
 @AutoConfigureBefore(DataSourceAutoConfiguration.class)
+@MockBean(JavaMailSender.class)
 @Configuration
 @EnableJpaRepositories("com.kh013j.model.repository")
 public class CaffinnoApplicationTestConfiguration {
