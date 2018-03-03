3d5ec18d11474cc09c588e441e8b58f50aa6a076
Arthur <freshsok@gmail.com>
2018-03-03 14:54:05 +0200

    Submission of each dish

--- src/main/java/com/kh013j/model/domain/Order.java
+++ src/main/java/com/kh013j/model/domain/Order.java
@@ -23,7 +23,7 @@ public class Order {
     private long id;
     @Past
     private Timestamp time;
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id")
     private User user;
     private int tablenumber;
