a06cfafaa0b4d680c9bd2b4839e1024c9529b81c
elena-maximenko <amely.honey@gmail.com>
2018-03-14 22:32:42 +0200

    KHJAV-85 Added previous changes again because of wrong pull

--- src/test/java/com/kh013j/RoleServiceImplTest.java
+++ src/test/java/com/kh013j/RoleServiceImplTest.java
@@ -1,9 +1,6 @@
 package com.kh013j;
 
-import com.kh013j.model.domain.Category;
 import com.kh013j.model.domain.Role;
-import com.kh013j.model.exception.CategoryNotFound;
-import com.kh013j.model.service.interfaces.CategoryService;
 import com.kh013j.model.service.interfaces.RoleService;
 import org.junit.Assert;
 import org.junit.Test;
@@ -22,64 +19,17 @@ public class RoleServiceImplTest {
     @Autowired
     private RoleService roleService;
 
-    @Test
-    public void create() {
-        Role role = new Role();
-        role.setName("Schweizer");
-
-        Role createdRole = roleService.create(role);
-        assertEquals(createdRole.getName(), role.getName());
-    }
-
     @Test(expected = Exception.class)
     public void tryToCreate() {
         roleService.create(new Role());
     }
 
-    @Test
-    public void delete() {
-        Role role = new Role();
-
-        Role createdRole = roleService.create(role);
-
-        Role deletedRole = roleService.delete(createdRole.getId());
-
-        Assert.assertNull(roleService.findByName(deletedRole.getName()));
-    }
 
     @Test(expected = Exception.class)
     public void tryToDelete(){
         roleService.delete(-1L);
     }
 
-    @Test
-    public void update() {
-        String name = "noName";
-
-        Role role  = new Role();
-        role.setName(name);
-
-        roleService.create(role);
-
-        Role foundRole = roleService.findByName(name);
-        foundRole.setName("Driver");
-
-        Role updatedRole = roleService.update(foundRole);
-
-        Role foundUpdatedRole = roleService.findByName(updatedRole.getName());
-
-        assertEquals(foundRole, foundUpdatedRole);
-    }
-
-    @Test
-    public void tryToUpdate() {
-        String name = "CEO";
-
-        Role role = new Role();
-        Role updatedRole= roleService.update(role);
-
-        assertEquals(updatedRole.getName(), name);
-    }
 
     @Test
     public void findRoleByName() {
