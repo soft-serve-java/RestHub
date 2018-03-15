bd65168f5ad0914b07c36a630ba37730a1ae74fd
Aleksei Volotka <ribalka@ukr.net>
2018-03-15 17:01:04 +0200

    routing, modules

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import {AdminComponent} from "./admin/admin.component";
+import {AdminUserComponent} from "./admin-user/admin-user.component";
 import {WaiterTablesComponent} from "./waiter-tables/waiter-tables.component";
 
 
 const routes: Routes = [
+  {path: 'welcome', component: WelcomeComponent},
+  {path: 'admin', component: AdminComponent},
+  {path: 'admin/user/all', component: AdminUserComponent},
   {path: 'welcome', component: WelcomeComponent},
   {path: 'waiter', component: WaiterTablesComponent}
 
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -9,14 +9,20 @@ import {WelcomeService} from "./services/welcome.service";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 import { AppService } from './services/app.service';
 import { environment } from '../environments/environment';
+import {AdminComponent} from "./admin/admin.component";
+import {AdminUserComponent} from "./admin-user/admin-user.component";
 import { WaiterTablesComponent } from './waiter-tables/waiter-tables.component';
 import {WaiterService} from "./services/waiter.service";
+import {AdminUserService} from "./services/admin-user.service";
 
 export const API_URL = new InjectionToken<string>('apiUrl');
 
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomeComponent,
+    AdminComponent,
+    AdminUserComponent,
     WelcomeComponent,
     WaiterTablesComponent
   ],
@@ -25,7 +31,7 @@ export const API_URL = new InjectionToken<string>('apiUrl');
     AppRoutingModule,
     HttpClientModule,
   ],
-  providers: [WelcomeService,WaiterService , HttpClient, AppService, {provide: "API_URL", useValue: environment.apiUrl}],
+  providers: [WelcomeService, HttpClient, AppService, AdminUserService, {provide: "API_URL", useValue: environment.apiUrl}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
