fe3c03410efd393f068fcdd48a5faa67369c7c74
Aleksei Volotka <ribalka@ukr.net>
2018-03-15 16:58:15 +0200

    admin-user service

---
+++ front-end/src/app/services/admin-user.service.ts
@@ -0,0 +1,15 @@
+import {Inject, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Category} from "../models/category";
+import {Observable} from "rxjs/Observable";
+import {User} from "../models/user";
+
+@Injectable()
+export class AdminUserService {
+
+  constructor(@Inject('API_URL') private userApi: string, private http:HttpClient) { }
+
+  getUsers(): Promise<User[]>{
+    return this.http.get<User[]>(this.userApi + "admin/user/all").toPromise();
+  }
+}
