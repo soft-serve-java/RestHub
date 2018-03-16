10309a8e0648253ccde94989cdbc2f5b43dc075a
Aleksei Volotka <ribalka@ukr.net>
2018-03-16 10:19:22 +0200

    app modules routing enviroment

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import {AdminComponent} from "./admin/admin.component";
 import {AdminUserComponent} from "./admin-user/admin-user.component";
 import {WaiterTablesComponent} from "./waiter-tables/waiter-tables.component";
-import { MenuComponent } from './menu/menu.component';
-import {DishPageComponent} from "./dish-page/dish-page.component";
+import {AdminDishComponent} from "./admin-dish/admin-dish.component";
 
 
 const routes: Routes = [
@@ -14,10 +13,9 @@ const routes: Routes = [
   {path: 'admin/user/all', component: AdminUserComponent},
   {path: 'welcome', component: WelcomeComponent},
   {path: 'waiter', component: WaiterTablesComponent},
+  {path: 'admin/dish/all', component: AdminDishComponent},
+  {path: 'waiter', component: WaiterTablesComponent},
   {path: 'welcome', component: WelcomeComponent},
-  {path: 'menu/:category/:page', component: MenuComponent},
-  {path: 'menu/:category', component: MenuComponent},
-  {path: 'dish/:id', component: DishPageComponent}
 ];
 
 @NgModule({
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -11,13 +11,9 @@ import { AppService } from './services/app.service';
 import { environment } from '../environments/environment';
 import {AdminComponent} from "./admin/admin.component";
 import {AdminUserComponent} from "./admin-user/admin-user.component";
-import { WaiterTablesComponent } from './waiter-tables/waiter-tables.component';
-import {WaiterService} from "./services/waiter.service";
 import {AdminUserService} from "./services/admin-user.service";
-import { MenuComponent } from './menu/menu.component';
-import { MenuService } from './services/menu.service';
-import { StorageServiceModule} from 'angular-webstorage-service';
-import { DishPageComponent } from './dish-page/dish-page.component';
+import {AdminDishComponent} from "./admin-dish/admin-dish.component";
+import {AdminDishService} from "./services/admin-dish.service";
 
 export const API_URL = new InjectionToken<string>('apiUrl');
 
@@ -27,20 +23,17 @@ export const API_URL = new InjectionToken<string>('apiUrl');
     WelcomeComponent,
     AdminComponent,
     AdminUserComponent,
+    AdminDishComponent,
     WelcomeComponent,
-    WaiterTablesComponent,
     WelcomeComponent,
-    MenuComponent,
-    DishPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserModule,
-    StorageServiceModule
   ],
-  providers: [WelcomeService, HttpClient, AppService, MenuService, AdminUserService,
+  providers: [WelcomeService, HttpClient, AppService, AdminUserService, AdminDishService,
      {provide: "API_URL", useValue: environment.apiUrl}
     ],
   bootstrap: [AppComponent]
