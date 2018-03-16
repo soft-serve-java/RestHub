c53f8b47cefc89cf4fedd00a204ee74fc8f63c62
Aleksei Volotka <ribalka@ukr.net>
2018-03-16 10:03:58 +0200

    adminDish service

---
+++ front-end/src/app/services/admin-dish.service.ts
@@ -0,0 +1,15 @@
+import {Inject, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Dish} from "../models/dish";
+
+
+
+@Injectable()
+export class AdminDishService {
+
+  constructor(@Inject('API_URL') private dishApi: string, private http:HttpClient){}
+
+  getDish(): Promise<Dish[]>{
+    return this.http.get<Dish[]>(this.dishApi + "admin/dish/all").toPromise();
+}
+}
