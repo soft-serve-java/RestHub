23ac16a9d48e6c73851b7e3acb89cd44e1c1b031
elena-maximenko <amely.honey@gmail.com>
2018-03-19 15:39:04 +0200

    KHJAV-96 Added router configuration for cook.

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import {CookComponent} from "./cook/cook.component";
 
 
 const routes: Routes = [
-  {path: 'welcome', component: WelcomeComponent}
+  {path: 'welcome', component: WelcomeComponent},
+  {path: 'cook', component: CookComponent}
 ];
 
 @NgModule({
