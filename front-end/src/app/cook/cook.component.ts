d7e82b88ea61823fa748aacdf5d331ece3ae0a58
elena-maximenko <amely.honey@gmail.com>
2018-03-19 16:23:51 +0200

    KHJAV-96 Added service for cook.

--- front-end/src/app/cook/cook.component.ts
+++ front-end/src/app/cook/cook.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import {OrderedDish} from "../models/orderedDish";
+import {Observable} from 'rxjs/Observable';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-cook',
   template: `
     <p>
-      cook works!
+      cook work!
     </p>
   `,
   styleUrls: ['./cook.component.css']
 })
 export class CookComponent implements OnInit {
-  orderedDishes: Array<OrderedDish>;
+   orderedDishes: Array<OrderedDish>;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  /*getOrderedDishes(): Observable<OrderedDish[]>{
+   return this.http.get('orderedDishes.json')
+  }*/
 
   ngOnInit() {
+    //this.getOrderedDishes();
   }
 
 }
