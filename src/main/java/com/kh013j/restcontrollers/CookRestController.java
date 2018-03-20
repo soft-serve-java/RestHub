a27dfa5a4aa613b68ead5ae532e7a7f48123cb2a
elena-maximenko <amely.honey@gmail.com>
2018-03-21 01:01:44 +0200

    Merged KHJAV-85 and KHJAV-96

--- src/main/java/com/kh013j/restcontrollers/CookRestController.java
+++ src/main/java/com/kh013j/restcontrollers/CookRestController.java
@@ -1,10 +1,12 @@
 package com.kh013j.restcontrollers;
 
 import com.kh013j.model.domain.OrderedDish;
+import com.kh013j.model.domain.Status;
 import com.kh013j.model.service.interfaces.OrderedDishService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,5 +24,28 @@ public class CookRestController {
     @GetMapping("api/cook")
     public List<OrderedDish> getOrderedDishes(){
         return orderedDishService.findAll();
+    }
+
+    @GetMapping("api/cook/{orderedDish}/{status}")
+    public void changeOrderedDishStatus(@PathVariable("orderedDish") OrderedDish orderedDish,
+                                        @PathVariable("status") Status status){
+       /* switch (status.getName()){
+            case Status.COOKING :{
+                orderedDishService.setCooking(orderedDish.getId());
+                break;
+            }
+            case Status.DONE:{
+                orderedDishService.setDone(orderedDish.getId());
+                break;
+            }
+        }*/
+
+       // in html href point to this method reference
+        if(status.getName().equals(Status.COOKING)){
+            orderedDishService.setCooking(orderedDish.getId());
+        }
+        if(status.getName().equals(Status.DONE)){
+            orderedDishService.setDone(orderedDish.getId());
+        }
     }
 }
