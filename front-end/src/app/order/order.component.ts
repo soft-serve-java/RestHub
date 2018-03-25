56a60d68a3eb82de5464db69b60c0c7c46e9eb3a
Arthur <freshsok@gmail.com>
2018-03-25 15:02:02 +0300

    Submitting order and getting result, also working with statuses

--- front-end/src/app/order/order.component.ts
+++ front-end/src/app/order/order.component.ts
@@ -2,8 +2,9 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {Dish} from "../models/dish";
 import {SESSION_STORAGE, WebStorageService} from 'angular-webstorage-service';
 import {OrderedDish} from "../models/orderedDish";
-import {Status} from "../models/status";
 import {StatusService} from "../services/status.service";
+import {OrderService} from "../services/order.service";
+import {Order} from "../models/order";
 
 @Component({
   selector: 'app-order',
@@ -14,14 +15,22 @@ export class OrderComponent implements OnInit {
 
   private dishes: Dish[];
 
-  private orderedDishes = [];
+  private newOrderedDishes = [];
 
   private orderMap: any;
 
+  private newOrder: Order;
+
+  private orderedDishes = [];
+
+  private tableNumber = 1;
+
   constructor(@Inject(SESSION_STORAGE) private storage: WebStorageService,
-              private statusService: StatusService) {
+              private statusService: StatusService,
+              private orderService: OrderService) {
     this.statusService.saveStatusesToLocalStorage();
     this.getDishes();
+    this.checkOrderByTableNumber();
   }
 
   ngOnInit() {
@@ -42,6 +51,30 @@ export class OrderComponent implements OnInit {
       orderedDish = new OrderedDish(dish, statusPreparing, temp.find(e => e.key == dish.id).value);
       array.push(orderedDish);
     });
-    this.orderedDishes = array;
+    this.newOrderedDishes = array;
   }
+
+  submitOrder(){
+    this.orderService.createOrder(this.newOrderedDishes).then(res =>{
+      this.newOrder = res;
+      this.getOrderedDishFromOrder();
+    } );
+  }
+
+  //Fix the method. Check if null and then add to storage.
+  getOrderedDishFromOrder(){
+    if (this.newOrder != null){
+      this.storage.set("orderedDish", this.newOrder.orderedFood);
+      this.orderedDishes = this.newOrder.orderedFood;
+    }
+  }
+
+  checkOrderByTableNumber(){
+    this.orderService.getOrderDetails(this.tableNumber).then(res => {
+      this.newOrder = res;
+      this.storage.set("orderedDish", this.newOrder.orderedFood);
+      this.orderedDishes = this.newOrder.orderedFood;
+    });
+  }
+
 }
--- src/main/java/com/kh013j/restcontrollers/OrderRestController.java
+++ src/main/java/com/kh013j/restcontrollers/OrderRestController.java
@@ -1,18 +1,38 @@
 package com.kh013j.restcontrollers;
 
 import com.kh013j.model.domain.Order;
+import com.kh013j.model.domain.OrderedDish;
 import com.kh013j.model.service.interfaces.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/order")
 @CrossOrigin("*")
 public class OrderRestController {
-    @Autowired
     private OrderService orderService;
-    @GetMapping(value = "/{id}")
+
+    @Autowired
+    public OrderRestController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping(value = "/table/{id}")
     public Order getOrderDetails(@PathVariable("id") int id){
         return orderService.findByTable(id);
     }
+
+    @PostMapping
+    public Order saveOrder(@RequestBody List<OrderedDish> orderedDishes){
+        Order order = new Order();
+        order.setTime(new Timestamp(new Date().getTime()));
+        order.setOrderedFood(orderedDishes);
+        order.setTablenumber(1);
+        return orderService.create(order);
+    }
+
 }
