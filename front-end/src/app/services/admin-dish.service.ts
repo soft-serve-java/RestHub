abf10df3d15e74c2b409b238c2952fce06c2374d
Aleksei Volotka <ribalka@ukr.net>
2018-03-28 10:41:32 +0300

    admin crud

--- front-end/src/app/services/admin-dish.service.ts
+++ front-end/src/app/services/admin-dish.service.ts
@@ -1,8 +1,8 @@
 import {Inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Dish} from "../models/dish";
-
-
+import {Category} from "../models/category";
+import {User} from "../models/user";
 
 @Injectable()
 export class AdminDishService {
@@ -11,12 +11,32 @@ export class AdminDishService {
 
   getDish(): Promise<Dish[]>{
     return this.http.get<Dish[]>(this.dishApi + "admin/dish/all").toPromise();
-}
+  }
 
   deleteDish(id: number): Promise<any>{
-      return this.http
-        .delete(this.dishApi + "admin/dish/delete/" + id )
-        .toPromise()
-        .then()
+    return this.http
+      .delete(this.dishApi + "admin/dish/delete/" + id )
+      .toPromise()
+      .then()
+  }
+
+
+  addDish(name: string, description: string, weight: number, category: Category, calories: number, preparingtime: number, price: number): Promise<any>{
+    return this.http
+    //.post(this.dishApi + "admin/dish/add", {dish: new Dish ()})
+      .post(this.dishApi + "admin/dish/add", {name, description, weight, category, calories, preparingtime, price})
+      .toPromise()
+      .then()
+  }
+
+
+  editDish(dish: Dish): Promise<any>{
+    return this.http
+      .post(this.dishApi + "admin/dish/add", dish)
+      .toPromise()
+  }
+
+  getDishById(id: number): Promise<Dish>{
+    return this.http.get<Dish>(this.dishApi + "admin/dish/" + id).toPromise();
   }
 }
