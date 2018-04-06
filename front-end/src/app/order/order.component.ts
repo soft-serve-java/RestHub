88875812b074dc7e6d09915ea2fb44d8866c7a3e
elena-maximenko <amely.honey@gmail.com>
2018-04-07 00:18:49 +0300

    KHJAV-104 Got wish data from popup.

--- front-end/src/app/order/order.component.ts
+++ front-end/src/app/order/order.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {OrderedDish} from "../models/orderedDish";
 import {StatusService} from "../services/status.service";
 import {OrderService} from "../services/order.service";
@@ -6,7 +6,7 @@ import {Order} from "../models/order";
 import {Status} from "../models/status";
 import {OrderStorageService} from "../services/order-storage.service";
 import {ActivatedRoute} from "@angular/router";
-import {MatDialog} from '@angular/material';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material';
 import {WishComponent} from "../wish/wish.component";
 
 @Component({
@@ -26,12 +26,14 @@ export class OrderComponent implements OnInit {
 
   private orderedDishes = [];
 
+  private wish: String;
 
   constructor(private route: ActivatedRoute,
               private statusService: StatusService,
               private orderService: OrderService,
               private orderStorageService: OrderStorageService,
-              public dialog: MatDialog) {
+              public dialog: MatDialog,
+             /* @Inject(MAT_DIALOG_DATA) public data: any*/) {
     this.statusService.saveStatusesToLocalStorage();
     this.getDishes();
     this.checkOrderByTableNumber();
@@ -41,18 +43,18 @@ export class OrderComponent implements OnInit {
   ngOnInit() {
   }
 
-  getDishes(){
+  getDishes() {
     this.dishes = this.orderStorageService.orderDishes;
     this.orderMap = this.orderStorageService.orderMap;
     this.dishesToOrderedDishes();
   }
 
-  dishesToOrderedDishes(){
+  dishesToOrderedDishes() {
     let temp = this.orderMap;
     let array = [];
     let preparingStatus = this.statusService.getPreparingStatus();
-    if(this.dishes == null) return;
-    this.dishes.forEach(function(dish){
+    if (this.dishes == null) return;
+    this.dishes.forEach(function (dish) {
       let orderedDish;
       orderedDish = new OrderedDish(dish, preparingStatus, temp.find(e => e.key == dish.id).value);
       array.push(orderedDish);
@@ -60,53 +62,53 @@ export class OrderComponent implements OnInit {
     this.newOrderedDishes = array;
   }
 
-  submitOrder(){
-    this.orderService.createOrder(this.newOrderedDishes).then(res =>{
+  submitOrder() {
+    this.orderService.createOrder(this.newOrderedDishes).then(res => {
       this.newOrder = res;
       this.getOrderedDishFromOrder();
       this.cleanUpAfterSubmission();
       this.orderedDishes = res.orderedFood;
-    } );
+    });
   }
 
-  submitOne(orderedDish: OrderedDish){
-    this.orderService.submitOne(orderedDish).then(res=>{
+  submitOne(orderedDish: OrderedDish) {
+    this.orderService.submitOne(orderedDish).then(res => {
       this.newOrder = res;
       this.remove(orderedDish);
       this.orderedDishes = res.orderedFood;
     })
   }
 
-  getOrderedDishFromOrder(){
-    if (this.newOrder != null){
+  getOrderedDishFromOrder() {
+    if (this.newOrder != null) {
       this.orderStorageService.orderDishes = this.newOrder.orderedFood;
       this.orderedDishes = this.newOrder.orderedFood;
     }
   }
 
-  checkOrderByTableNumber(){
+  checkOrderByTableNumber() {
     this.orderService.getOrderDetailsByTable().then(res => {
       this.newOrder = res;
       this.orderedDishes = this.newOrder.orderedFood;
     });
   }
 
-  remove(orderItem: OrderedDish){
+  remove(orderItem: OrderedDish) {
     this.newOrderedDishes.splice(this.newOrderedDishes.indexOf(orderItem), 1);
 
     let temp = this.orderStorageService.orderMap;
 
-    temp.map(function(elem, index, array){
-        if(elem.key == orderItem.dish.id){
-          array.splice(index, 1);
-        }
+    temp.map(function (elem, index, array) {
+      if (elem.key == orderItem.dish.id) {
+        array.splice(index, 1);
+      }
     });
     this.orderStorageService.orderMap = temp;
 
     temp = this.orderStorageService.orderDishes;
     console.log(this.orderStorageService.orderDishes);
-    temp.map(function(elem, index, array){
-      if(elem.id == orderItem.dish.id){
+    temp.map(function (elem, index, array) {
+      if (elem.id == orderItem.dish.id) {
         array.splice(index, 1);
       }
     });
@@ -114,64 +116,68 @@ export class OrderComponent implements OnInit {
 
   }
 
-  getTotalAmount(): number{
+  getTotalAmount(): number {
     let totalAmount = 0;
     this.newOrderedDishes.forEach(item => totalAmount += item.dish.price * item.quantity);
     this.orderedDishes.forEach(item => totalAmount += item.dish.price * item.quantity);
     return totalAmount;
   }
 
-  incrementQuantity(orderedDish: OrderedDish){
+  incrementQuantity(orderedDish: OrderedDish) {
     orderedDish.quantity++;
     let temp = this.orderStorageService.orderMap;
     temp.map(elem => {
-      if (elem.key == orderedDish.dish.id){
+      if (elem.key == orderedDish.dish.id) {
         elem.value++;
       }
     });
     this.orderStorageService.orderMap = temp;
   }
 
-  decrementQuantity(orderedDish: OrderedDish){
+  decrementQuantity(orderedDish: OrderedDish) {
     orderedDish.quantity--;
     let temp = this.orderStorageService.orderMap;
     temp.map(elem => {
-      if (elem.key == orderedDish.dish.id){
+      if (elem.key == orderedDish.dish.id) {
         elem.value--;
       }
     });
     this.orderStorageService.orderMap = temp;
   }
 
-  getClassByStatus(status: Status):string{
-    if(status.name == 'preparing') return 'table-info';
-    if(status.name == 'cooking')   return 'table-warning';
-    if(status.name == 'delivery')  return 'table-success';
+  getClassByStatus(status: Status): string {
+    if (status.name == 'preparing') return 'table-info';
+    if (status.name == 'cooking') return 'table-warning';
+    if (status.name == 'delivery') return 'table-success';
     return '';
   }
 
-  isDiabledByQuantity(orderItem: OrderedDish):boolean{
+  isDiabledByQuantity(orderItem: OrderedDish): boolean {
     return orderItem.quantity <= 1;
   }
 
-  isDisabledByNewOrderedDishes():boolean{
+  isDisabledByNewOrderedDishes(): boolean {
     return this.newOrderedDishes.length < 1;
   }
 
-  private cleanUpAfterSubmission(){
+  private cleanUpAfterSubmission() {
     this.orderStorageService.removeStorage();
     this.dishes = [];
     this.orderMap = [];
     this.newOrderedDishes = [];
   }
 
-  showWishPopup(){
+  showWishPopup() {
+    console.log('showWishPopup()');
+
     let dialogRef = this.dialog.open(WishComponent);
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog closed: ${result}`);
-      //this.dialogResult = result;
+    dialogRef.componentInstance.onOk.subscribe(result => {
+      console.log('result = ' + result);
+      this.wish = result;
     });
-  }
+
+    console.log("wish = " + this.wish);
+    }
 
 }
